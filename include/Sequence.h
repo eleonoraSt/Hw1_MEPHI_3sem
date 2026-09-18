b82ae416bf5b6d7e42c949dc002b40d3e6580bc6
eleonoraSt <eas402me@gmail.com>
2026-09-18 15:19:16 +0300

    removed back references in Sequence

--- include/Sequence.h
+++ include/Sequence.h
@@ -6,11 +6,6 @@
 template <class T>
 class Sequence {
 public:
-    /*
-     * Ссылка возвращается, когда редактируется данный экземпляр
-     * Указатель возвращается, когда создаётся новый экземпляр
-     */
-
     virtual ~Sequence() {}
 
     virtual T GetFirst() const = 0;
@@ -19,10 +14,10 @@ public:
     virtual Sequence<T>* GetSubsequence(size_t startIndex, size_t endIndex) const = 0;
     virtual size_t GetLength() const = 0;
 
-    virtual Sequence<T>& Append(T item) = 0;
-    virtual Sequence<T>& Prepend(T item) = 0;
-    virtual Sequence<T>& InsertAt(T item, size_t index) = 0;
-    virtual Sequence<T>& Concat(Sequence<T>& list) = 0;
+    virtual Sequence<T>* Append(T item) = 0;
+    virtual Sequence<T>* Prepend(T item) = 0;
+    virtual Sequence<T>* InsertAt(T item, size_t index) = 0;
+    virtual Sequence<T>* Concat(Sequence<T>& list) = 0;
 
     virtual Sequence<T>* Map(T (*func)(T)) const = 0;
     virtual Sequence<T>* Where(bool (*func)(T)) const = 0;
